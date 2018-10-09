07887bf962ecec9655ad01af0ca15efcecaea3db
war-turtle <kartikyadav71199@gmail.com>
2018-10-09 02:10:21 -0700

    Nav bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
 import OnBoarding from './components/OnBoarding/OnBoarding';
+import Desktop from './components/Desktop/Desktop';
+import Events from './components/Events/Events';
+import Guests from './components/Guests/Guests';
+import Contact from './components/ContactUs/ContactUs';
+import AboutUs from './components/AboutUs/AboutUs';
+import Sponsers from './components/Sponsers/Sponsers';
+import User from './components/User/User';
 import LoggedIn from './LoggedIn';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <div className="App">
-        <Route exact path="/" component={LoggedIn} />
         <Route path="/onBoarding" component={OnBoarding} />
+        <LoggedIn exact path="/" component={Desktop} />
+        <LoggedIn path="/events" component={Events} />
+        <LoggedIn path="/guestLectures" component={Guests} />
+        <LoggedIn path="/contactUs" component={Contact} />
+        <LoggedIn path="/aboutUs" component={AboutUs} />
+        <LoggedIn path="/sponsers" component={Sponsers} />
+        <LoggedIn path="/user" component={User} />
       </div>
     </Switch>
   </BrowserRouter>
--- src/LoggedIn.jsx
+++ src/LoggedIn.jsx
@@ -1,28 +1,19 @@
-
-
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
-import Desktop from './components/Desktop/Desktop';
-import Events from './components/Events/Events';
-import Guests from './components/Guests/Guests';
-import Contact from './components/ContactUs/ContactUs';
-import AboutUs from './components/AboutUs/AboutUs';
-import Sponsers from './components/Sponsers/Sponsers';
-import User from './components/User/User';
 import DOC from './components/Doc/Doc';
 
-const LoggedIn = () => (
-  <div className="loggedIn">
-    <Nav />
-    <Route exact path="/" component={Desktop} />
-    <Route path="/events" component={Events} />
-    <Route path="/guestLectures" component={Guests} />
-    <Route path="/contactUs" component={Contact} />
-    <Route path="/aboutUs" component={AboutUs} />
-    <Route path="/sponsers" component={Sponsers} />
-    <Route path="/user" component={User} />
-    <DOC />
-  </div>
+const LoggedIn = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={matchProps => (
+      <div>
+        <Nav />
+        <Component {...matchProps} />
+        <DOC />
+      </div>
+    )}
+  />
 );
 
 export default LoggedIn;
