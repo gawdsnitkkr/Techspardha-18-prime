8e098193314908f3bfea6c2c4d5ad90c459cc087
anuj <anujjangra25119@gmail.com>
2018-10-09 23:07:38 +0530

    fetched data

--- src/components/Finder/Finder.jsx
+++ src/components/Finder/Finder.jsx
@@ -1,113 +1,63 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import $ from 'jquery';
 import './Finder.css';
+import actions from '../../actions';
+
+const Folder = () => {
+  console.log('folder');
+  return (
+    <div className="col-6 col-sm-3 my-4 folder">
+      <Link to="/events" className="event-link">
+        <img src="/images/folder2.svg" style={{ width: '30%' }} alt="img" />
+        <span className="folder-text">
+              Event 1
+        </span>
+      </Link>
+    </div>
+  );
+};
 
 class Finder extends Component {
-  componentDidMount() {
+  componentDidMount = () => {
     $('.event-link').click(() => {
       $('.Finder').fadeToggle(200);
     });
+
+    const { getEvents } = this.props;
+    getEvents();
   }
 
-  render() {
-    return (
-      <div className="main-container">
-
-
-        <div className="row appsContainerDiv_data">
-          <div className="col-12">
-            <div className="container my-5">
-              <div className="row search justify-content-center">
-                <input type="text" placeholder="Search.." />
-              </div>
-              <hr />
-
-              <div className="row apps my-3">
-                <div className="col-6 col-sm-3 my-4 folder">
-                  <Link to="/events" className="event-link">
-                    <img src="/images/folder2.svg" style={{ width: '30%' }} alt="img" />
-                    <span className="folder-text">
-              Event 1
-                    </span>
-                  </Link>
-                </div>
-                <div className="col-6 col-sm-3 my-4 folder">
-                  <img src="/images/folder2.svg" style={{ width: '30%' }} alt="img" />
-                  <span className="folder-text">
-            Event 1
-                  </span>
-                </div>
-                <div className="col-6 col-sm-3 my-4 folder">
-                  <img src="/images/folder2.svg" style={{ width: '30%' }} alt="img" />
-                  <span className="folder-text">
-          Event 1
-                  </span>
-                </div>
-                <div className="col-6 col-sm-3 my-4 folder">
-                  <img src="/images/folder2.svg" style={{ width: '30%' }} alt="img" />
-                  <span className="folder-text">
-          Event 1
-                  </span>
-                </div>
-                <div className="col-6 col-sm-3 my-4 folder">
-                  <img src="/images/folder2.svg" style={{ width: '30%' }} alt="img" />
-                  <span className="folder-text">
-           Event 1
-                  </span>
-                </div>
-                <div className="col-6 col-sm-3 my-4 folder">
-                  <img src="/images/folder2.svg" style={{ width: '30%' }} alt="img" />
-                  <span className="folder-text">
-           Event 1
-                  </span>
-                </div>
-                <div className="col-6 col-sm-3 my-4 folder">
-                  <img src="/images/folder2.svg" style={{ width: '30%' }} alt="img" />
-                  <span className="folder-text">
-          Event 1
-                  </span>
-                </div>
-                <div className="col-6 col-sm-3 my-4 folder">
-                  <img src="/images/folder2.svg" style={{ width: '30%' }} alt="img" />
-                  <span className="folder-text">
-           Event 1
-                  </span>
-                </div>
-                <div className="col-6 col-sm-3 my-4 folder">
-                  <img src="/images/folder2.svg" style={{ width: '30%' }} alt="img" />
-                  <span className="folder-text">
-            Event 1
-                  </span>
-                </div>
-                <div className="col-6 col-sm-3 my-4 folder">
-                  <img src="/images/folder2.svg" style={{ width: '30%' }} alt="img" />
-                  <span className="folder-text">
-            Event 1
-                  </span>
-                </div>
-                <div className="col-6 col-sm-3 my-4 folder">
-                  <img src="/images/folder2.svg" style={{ width: '30%' }} alt="img" />
-                  <span className="folder-text">
-            Event 1
-                  </span>
-                </div>
-                <div className="col-6 col-sm-3 my-4 folder">
-                  <img src="/images/folder2.svg" style={{ width: '30%' }} alt="img" />
-                  <span className="folder-text">
-            Event 1
-                  </span>
-                </div>
-              </div>
-
+  render = () => (
+    <div className="main-container">
+      <div className="row appsContainerDiv_data">
+        <div className="col-12">
+          <div className="container my-5">
+            <div className="row search justify-content-center">
+              <input type="text" placeholder="Search.." />
+            </div>
+            <hr />
+            <div className="row apps my-3">
+              <Folder />
             </div>
           </div>
         </div>
-
       </div>
 
-    );
-  }
+    </div>
+  );
 }
 
-export default Finder;
+const mapStateToProps = state => ({
+  events: state.events,
+});
+
+
+const mapDispatchToProps = dispatch => ({
+  getEvents: () => {
+    dispatch(actions.getEvents());
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Finder);
