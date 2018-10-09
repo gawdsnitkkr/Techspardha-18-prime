177000c645cb11290f75532688fc4b17041ad4bc
anuj <anujjangra25119@gmail.com>
2018-10-09 20:26:43 +0530

    added alt tags

--- src/components/Doc/Doc.jsx
+++ src/components/Doc/Doc.jsx
@@ -8,32 +8,32 @@ const DOC = () => (
       <div className="btn-group bg-light" role="group" aria-label="dockIcons">
         <button type="button" className="btn dock-icon-btn" data-toggle="tooltip" data-placement="top" title="About us">
           <Link to="/aboutUs">
-            <img src="/images/doc/aboutus.png" className="dock-icon" />
+            <img src="/images/doc/aboutus.png" className="dock-icon" alt="icon" />
           </Link>
         </button>
         <button type="button" className="btn dock-icon-btn" data-toggle="tooltip" data-placement="top" title="Exhibitions">
           <a href="#exhibition">
-            <img src="/images/doc/exhibition.png" className="dock-icon" />
+            <img src="/images/doc/exhibition.png" className="dock-icon" alt="icon" />
           </a>
         </button>
         <button type="button" className="btn dock-icon-btn" data-toggle="tooltip" data-placement="top" title="Guest lecture">
           <Link to="/guestLectures">
-            <img src="/images/doc/guest.png" className="dock-icon" />
+            <img src="/images/doc/guest.png" className="dock-icon" alt="icon" />
           </Link>
         </button>
         <button type="button" className="btn dock-icon-btn" data-toggle="tooltip" data-placement="top" title="Hackathon">
           <a href="https://techspardha.org/Hackathon/" target="_blank">
-            <img src="/images/doc/hackathon.png" className="dock-icon" />
+            <img src="/images/doc/hackathon.png" className="dock-icon" alt="icon" />
           </a>
         </button>
         <button type="button" className="btn dock-icon-btn" data-toggle="tooltip" data-placement="top" title="Sponsors">
           <Link to="sponsers">
-            <img src="/images/doc/sponsor.png" className="dock-icon" />
+            <img src="/images/doc/sponsor.png" className="dock-icon" alt="icon" />
           </Link>
         </button>
         <button type="button" className="btn dock-icon-btn" data-toggle="tooltip" data-placement="top" title="Contact us">
           <Link to="/contactUs">
-            <img src="/images/doc/contact.png" className="dock-icon" />
+            <img src="/images/doc/contact.png" className="dock-icon" alt="icon" />
           </Link>
         </button>
       </div>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -17,96 +17,82 @@ class Nav extends React.Component {
       <div>
         <nav className="navbar fixed-top navbar-light bg-light">
           <a className="navbar-brand" id="navbar-finder-button">
-	techOS
-
+            techOS
           </a>
           <div className="nav-item dropdown">
             <a className="nav-link dropdown-toggle" href="#" id="navbarTimelineDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					Timeline
-
+             Timeline
             </a>
             <div className="dropdown-menu" aria-labelledby="navbarTimelineDropdown" id="timelineDropdown">
               <h2>
-	October 26, 2018
-
+	             October 26, 2018
               </h2>
               <h5>
-	Friday
-
+              Friday
               </h5>
               <h2>
-	10:00
-
+              10:00
                 <small>
-	AM
-
+                AM
                 </small>
               </h2>
               <ul>
                 <li>
                   <h6>
                     <a href="#event1">
-	Event 1
-
+                    Event 1
                     </a>
                   </h6>
                   <p>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
-
-
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam
                   </p>
                 </li>
                 <li>
                   <h6>
                     <a href="#event2">
-	Event 1
-
+                    Event 1
                     </a>
                   </h6>
                   <p>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
-
-
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam
                   </p>
                 </li>
                 <li>
                   <h6>
                     <a href="#event3">
-	Event 1
-
+                    Event 1
                     </a>
                   </h6>
                   <p>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
-
-
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam
                   </p>
                 </li>
               </ul>
               <a href="#fullTimeline">
-	See full timeline
-
+              See full timeline
               </a>
             </div>
           </div>
           <div className="nav-item dropdown">
             <a className="nav-link dropdown-toggle" href="#" id="navbarUserOptionsDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					John Appleseed
-
+            John Appleseed
             </a>
             <div className="dropdown-menu" aria-labelledby="navbarUserOptionsDropdown" id="userDropdown">
               <Link className="dropdown-item" to="/user">
-	My Profile
-
+              My Profile
               </Link>
               <a className="dropdown-item" href="#userLogout">
-	Log Out
-
+              Log Out
               </a>
             </div>
           </div>
         </nav>
-
         <div className="Finder">
           <Finder />
         </div>
