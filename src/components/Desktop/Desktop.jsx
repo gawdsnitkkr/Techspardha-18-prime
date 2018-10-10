f7ebbc62acd0f1328279222f1810b1255f00bcbf
war-turtle <kartikyadav71199@gmail.com>
2018-10-10 01:00:40 -0700

    new Desktop and Finder added

--- src/components/Desktop/Desktop.jsx
+++ src/components/Desktop/Desktop.jsx
@@ -11,15 +11,45 @@ class Desktop extends React.Component {
 
   render = () => {
     const { fact } = this.props;
+
     return (
-      <div id="desktop">
-        <h1>
-        Techspardha Prime
-          <br />
-          {' '}
-          {fact}
-        </h1>
+      <div className="jumbotron" id="desktop-page">
+        <div className="container text-center" id="desktop-center">
+          <h1 className="display-2" id="main-heading">
+Techspardha'18
+            {' '}
+            <small className="text-muted">
+Prime
+            </small>
+          </h1>
+          <h2 className="mb-4">
+Come join us this October
+          </h2>
+
+          <div className="row justify-content-md-center">
+            <div className="col-sm-4">
+              <button type="button" className="btn btn-danger">
+Sign in
+              </button>
+            </div>
+          </div>
+
+          <hr className="my-4" />
+          <blockquote className="blockquote text-center">
+            <p className="mb-0 lead">
+              {fact}
+            </p>
+            <footer className="blockquote-footer">
+              <cite title="Source Title">
+Team Techspardha
+              </cite>
+            </footer>
+          </blockquote>
+          <hr className="my-4" />
+
+        </div>
       </div>
+
     );
   }
 }
