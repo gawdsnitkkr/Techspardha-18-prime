e226b32d45265585781e94480e2137859e2d8b73
war-turtle <kartikyadav71199@gmail.com>
2018-10-17 22:53:30 +0530

    cards are now almost equal

--- src/components/Guests/Guests.jsx
+++ src/components/Guests/Guests.jsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import $ from 'jquery';
 import actions from '../../actions';
 import './Guests.css';
+
+const DescriptionModal = () => (
+  <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div className="modal-dialog" role="document">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h5 className="modal-title" id="exampleModalLabel">
+            Description
+          </h5>
+          <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">
+              &times;
+            </span>
+          </button>
+        </div>
+        <div className="modal-body">
+          <p />
+        </div>
+        <div className="modal-footer">
+          <button type="button" className="btn btn-secondary" data-dismiss="modal">
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 const GuestsCard = (props) => {
   const {
     name, date, imageUrl, time, desc,
   } = props.lectures;
+
+  const onClickViewMore = (desc) => {
+    $('.modal-body>p').html(desc);
+  };
+
   return (
     <div className="col-sm-4">
       <div className="card">
@@ -34,8 +67,21 @@ const GuestsCard = (props) => {
           <h5>
             About
           </h5>
+          <p className="card-text" style={{ height: '254px', marginBottom: '0px' }}>
+            {desc.substr(0, 200)}
+          </p>
           <p className="card-text">
-            {desc}
+            {
+              desc.length > 199 ? (
+                <div>
+                  <button type="button" className="btn btn-success" data-toggle="modal" data-target="#exampleModal" onClick={() => { onClickViewMore(desc); }}>
+                    View more
+                  </button>
+                </div>
+              ) : null
+            }
+
+            <DescriptionModal />
           </p>
         </div>
       </div>
